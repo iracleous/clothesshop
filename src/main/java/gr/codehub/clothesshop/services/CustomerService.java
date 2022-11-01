38884eee8a85914f03dfc86908d4f61552253681
dimitris <iracleous@gmail.com>
2022-11-01 10:22:30 +0200

    Initial commit after modeling

---
+++ src/main/java/gr/codehub/clothesshop/services/CustomerService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gr.codehub.clothesshop.services;
+
+import gr.codehub.clothesshop.model.Customer;
+import gr.codehub.clothesshop.model.Order;
+import gr.codehub.clothesshop.model.Product;
+import java.util.List;
+
+/**
+ *
+ * @author iracl
+ */
+public interface CustomerService {
+    
+    boolean register(Customer customer);
+    
+    boolean placeOrder(int customerId, int productId, int orderId);
+    
+    List<Product> searchProduct(String productName);
+    
+    Order showOrderint (int orderId);
+    
+    
+}
