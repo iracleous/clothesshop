ccc55cb9112819079972c6bac01febf9ec9d7929
dimitris <iracleous@gmail.com>
2022-11-07 13:26:37 +0200

    using lists

---
+++ src/main/java/gr/codehub/clothesshop/repository/ProductRepositoryImpl.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gr.codehub.clothesshop.repository;
+
+import gr.codehub.clothesshop.model.Product;
+import gr.codehub.clothesshop.model.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author iracl
+ */
+public class ProductRepositoryImpl implements ProductRepository{
+
+  private final List<Product> products;
+  private int index;  
+
+    public ProductRepositoryImpl() {
+        this.products = new ArrayList<>();
+    }
+    
+     
+    @Override
+    public int create(Product product) {
+            product.setId(index++);
+            products.add(product);
+            return product.getId();
+    }
+
+    @Override
+    public Product read(int productId) {
+        for (Product product:products){
+            if (product.getId() == productId)
+                return product;
+        }
+            return null;
+    }
+
+    @Override
+    public List<Product> read() {
+            return products;
+    
+    }
+
+    @Override
+    public void update(int productId, double price) {
+        Product product =  read(productId);
+        if(product !=null){
+            product.setPrice(price);
+        }
+    }
+
+    @Override
+    public boolean delete(int productId) {
+         Product product =  read(productId);
+        if(product !=null){
+            products.remove(product);
+            return true;
+        }
+        return false;
+        
+     }
+}
